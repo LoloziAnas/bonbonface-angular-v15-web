385804311d73fb2e88123b7c73c488602c014aee
LoloziAnas <lolozianas1@gmail.com>
2022-12-15 15:10:14 +0000

    add face snap service and implement it

--- src/app/face-bon-list/face-bon-list.component.ts
+++ src/app/face-bon-list/face-bon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FaceSnap} from "../models/FaceSnap";
+import {FaceSnapsService} from "../services/face-snaps.service";
 
 @Component({
   selector: 'app-face-bon-list',
@@ -9,31 +10,10 @@ import {FaceSnap} from "../models/FaceSnap";
 export class FaceBonListComponent implements OnInit {
 
   faceSnaps!: FaceSnap[];
-
+  constructor(private faceSnapsService: FaceSnapsService) {
+  }
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'My best dude since childhood.',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 0
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 0
-      }
-    ];
+    this.faceSnaps = this.faceSnapsService.faceSnaps;
   }
 
 }
