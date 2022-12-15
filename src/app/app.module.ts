4e92aef0874a78a79d4b8aae437b34b6776a2124
LoloziAnas <lolozianas1@gmail.com>
2022-12-15 14:55:23 +0000

    adding header component & implement it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FaceBonComponent } from './face-bon/face-bon.component';
 import { FaceBonListComponent } from './face-bon-list/face-bon-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceBonComponent,
-    FaceBonListComponent
+    FaceBonListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
