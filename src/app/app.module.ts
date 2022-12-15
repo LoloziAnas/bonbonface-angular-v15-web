4500845b8cc155e2162835a6a6e3cd1182791f13
LoloziAnas <lolozianas1@gmail.com>
2022-12-15 11:55:25 +0000

    redesign app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FaceBonComponent } from './face-bon/face-bon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceBonComponent
   ],
   imports: [
     BrowserModule
