d6160c6cf642863132e76f621dabb0b8958af29b
LoloziAnas <lolozianas1@gmail.com>
2022-12-16 16:15:53 +0000

    Adding New face bon component

---
+++ src/app/new-face-bon/new-face-bon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-face-bon',
+  templateUrl: './new-face-bon.component.html',
+  styleUrls: ['./new-face-bon.component.scss']
+})
+export class NewFaceBonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
