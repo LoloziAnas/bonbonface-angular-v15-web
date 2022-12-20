bc8ba21760ca24ef90dfcd3bb15146a15a7f0bf8
LoloziAnas <lolozianas1@gmail.com>
2022-12-20 16:45:52 +0000

    working with module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FaceBonListComponent} from "./face-bons/components/face-bon-list/face-bon-list.component";
 import {LoadingPageComponent} from "./loading-page/components/loading-page.component";
-import {FaceBonDetailComponent} from "./face-bons/components/face-bon-detail/face-bon-detail.component";
-import {NewFaceBonComponent} from "./face-bons/components/new-face-bon/new-face-bon.component";
 
 const routes: Routes = [
   { path: 'facesnaps', loadChildren:() => import('./face-bons/face-bons.module').then(m => m.FaceBonsModule)},
--- src/app/face-bons/face-bons-routing.module.ts
+++ src/app/face-bons/face-bons-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {SingleFaceBonComponent} from "./components/single-face-bon/single-face-bon.component";
 import {FaceBonListComponent} from "./components/face-bon-list/face-bon-list.component";
 import {NewFaceBonComponent} from "./components/new-face-bon/new-face-bon.component";
 
 
 const routes: Routes = [
+  {path: 'create', component: NewFaceBonComponent},
   {path: ':id', component: SingleFaceBonComponent},
-  {path: '', component: FaceBonListComponent},
-  {path: 'create', component: NewFaceBonComponent}
+  {path: '', component: FaceBonListComponent}
 ];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class FaceBonsRoutingModule { }
