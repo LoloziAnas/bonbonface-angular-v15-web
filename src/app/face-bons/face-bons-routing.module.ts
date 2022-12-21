5df0053f853ced8cab65c7e19d331143ddccb757
LoloziAnas <lolozianas1@gmail.com>
2022-12-21 11:13:22 +0000

    adding & implementing auth guard

--- src/app/face-bons/face-bons-routing.module.ts
+++ src/app/face-bons/face-bons-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {SingleFaceBonComponent} from "./components/single-face-bon/single-face-bon.component";
 import {FaceBonListComponent} from "./components/face-bon-list/face-bon-list.component";
 import {NewFaceBonComponent} from "./components/new-face-bon/new-face-bon.component";
+import {AuthGuard} from "../core/guards/auth.guard";
 
 
 const routes: Routes = [
-  {path: 'create', component: NewFaceBonComponent},
-  {path: ':id', component: SingleFaceBonComponent},
-  {path: '', component: FaceBonListComponent}
+  {path: 'create', component: NewFaceBonComponent, canActivate: [AuthGuard]},
+  {path: ':id', component: SingleFaceBonComponent, canActivate: [AuthGuard]},
+  {path: '', component: FaceBonListComponent, canActivate: [AuthGuard]}
 ];
 @NgModule({
   declarations: [],
